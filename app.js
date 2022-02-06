5bb79f4a201fc451efe51ec7c9b96f7359e44656
RoshinAb <roshinsrs@gmail.com>
2022-02-06 13:13:20 +0530

    New case-study2

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homerouter');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+});
+
+app.listen(process.env.PORT||5000,()=>{
+    console.log("Server Ready on 5000");
+});
